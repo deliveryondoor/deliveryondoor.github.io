66902bebdc8f1ebffdd6b04650fa51905353eaa3
Salokya Kumar <kumar.salokya007@gmail.com>
2022-01-30 16:35:07 +0530

    added help section

--- src/App.js
+++ src/App.js
@@ -17,15 +17,19 @@ function App() {
     const [show1, setShow1] = useState(false);
     const [show2, setShow2] = useState(false);
     const [show3, setShow3] = useState(false);
+    const [show4, setShow4] = useState(false);
 
-    const handleClose1 = () => setShow1(false);
     const handleShow1 = () => setShow1(true);
+    const handleClose1 = () => setShow1(false);
 
-    const handleClose2 = () => setShow2(false);
     const handleShow2 = () => setShow2(true);
+    const handleClose2 = () => setShow2(false);
 
-    const handleClose3 = () => setShow3(false);
     const handleShow3 = () => setShow3(true);
+    const handleClose3 = () => setShow3(false);
+
+    const handleShow4 = () => setShow4(true);
+    const handleClose4 = () => setShow4(false);
 
     function disableButton() {
         setTimeout(() => {
@@ -64,7 +68,7 @@ function App() {
 
                 if (name !== "" && address !== "" && landmark !== "" && ward !== "" && phone1 !== "" && items !== "") {
                     fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-                        .then(response => {
+                        .then(() => {
                             handleClose2();
                             handleShow3();
                         })
@@ -76,21 +80,13 @@ function App() {
 
     return (
         <div>
-            <Modal size="sm" centered show={show3} onHide={handleClose3}>
-                <Modal.Body>
-                    <p>Thank You for ordering. <br/> We will contact you soon...</p>
-                </Modal.Body>
-                <Button variant="success" onClick={handleClose3}>
-                    Okay
-                </Button>
-            </Modal>
-
-            <div data-aos="fade-right" class="home">
+            <div data-aos="fade-right" className="home">
                 {/*Navbar*/}
                 <Navbar bg="light" expand="lg">
                     <Container>
-                        <img class="logo-brand" src={logo} alt="logo"/>
+                        <img className="logo-brand" src={logo} alt="logo"/>
                         <Button onClick={handleShow1} data-aos="zoom-in" data-aos-delay="2000" variant="info"
+                                className="contact-btn"
                                 id="contact">Contact Us.</Button>{' '}
                     </Container>
                 </Navbar>
@@ -114,25 +110,60 @@ function App() {
                         className="tick2" icon={faCalendarCheck}/> 24x7 Facilities.</p>
                     <p data-aos="fade-up" data-aos-duration="1500" data-aos-delay="900"><FontAwesomeIcon
                         className="tick2" icon={faCalendarCheck}/> COD option.</p>
-                    <p data-aos="fade-up" data-aos-duration="1800" data-aos-delay="1300">So what you waiting for ?</p>
+                    <p data-aos="fade-up" data-aos-duration="1800" data-aos-delay="1300">So what are you waiting for
+                        ?</p>
                     <button onClick={handleShow2} data-aos="zoom-in" data-aos-delay="2500" type="button"
-                            className="btn btn-success">Order Now !
+                            className="btn btn-success">Order Now!
+                    </button>
+                    <button onClick={handleShow4} data-aos="zoom-in" data-aos-delay="2500" type="button"
+                            className="btn btn-warning help-btn">Help?
                     </button>
 
                     <Modal centered show={show2} onHide={handleClose2}>
                         <Modal.Header closeButton> Enter Your Details</Modal.Header>
                         <Modal.Body>
                             <form id="form" name="google-sheet">
-                                <input id="name" type="text" name="Name" className="form-control form-group" placeholder="Full Name *" required/>
-                                <input id="address" type="text" name="Address" className="form-control form-group" placeholder="Address *" required/>
-                                <input id="landmark" type="text" name="Landmark" className="form-control form-group" placeholder="Landmark *" required/>
-                                <input id="ward" type="number" name="Ward" className="form-control form-group" placeholder="Ward Number *" required/>
-                                <input id="phone1" type="number" name="Phone1" className="form-control form-group" placeholder="Contact Number 1*" required/>
-                                <input type="number" name="Phone2" className="form-control form-group" placeholder="Contact Number 2"/>
-                                <textarea id="items" name="Item"  className="form-control form-group" rows="3" placeholder="Enter items to be ordered..." required />
-                                <input id="datePicker" type="date" className="form-control form-group" name="Date" defaultValue={date}/>
-                                <input id="orderBtn" type="submit" name="submit" className="btn btn-primary order-btn" onClick={disableButton} value="Order Now"/>
+                                <input id="name" type="text" name="Name" className="form-control form-group"
+                                       placeholder="Full Name *" required/>
+                                <input id="address" type="text" name="Address" className="form-control form-group"
+                                       placeholder="Address *" required/>
+                                <input id="landmark" type="text" name="Landmark" className="form-control form-group"
+                                       placeholder="Landmark *" required/>
+                                <input id="ward" type="number" name="Ward" className="form-control form-group"
+                                       placeholder="Ward Number *" required/>
+                                <input id="phone1" type="number" name="Phone1" className="form-control form-group"
+                                       placeholder="Contact Number 1*" required/>
+                                <input type="number" name="Phone2" className="form-control form-group"
+                                       placeholder="Contact Number 2"/>
+                                <textarea id="items" name="Item" className="form-control form-group" rows="3"
+                                          placeholder="Enter items to be ordered..." required/>
+                                <input id="datePicker" type="date" className="form-control form-group" name="Date"
+                                       defaultValue={date}/>
+                                <input id="orderBtn" type="submit" name="submit" className="btn btn-primary order-btn"
+                                       onClick={disableButton} value="Order Now"/>
                             </form>
+                        </Modal.Body>
+                    </Modal>
+
+                    <Modal size="sm" centered show={show3} onHide={handleClose3}>
+                        <Modal.Body>
+                            <p style={{marginBottom:0}}>
+                                <center>We will deliver your product soon.<br/> Thank You!</center>
+                            </p>
+                        </Modal.Body>
+                        <Button className="order-ack" variant="success" onClick={handleClose3}>
+                            Okay
+                        </Button>
+                    </Modal>
+
+                    <Modal size="lg" centered show={show4} onHide={handleClose4}>
+                        <Modal.Header closeButton>Placing an Order...</Modal.Header>
+                        <Modal.Body>
+                            <iframe className="video-frame" src="https://www.youtube.com/embed/K1QICrgxTjA"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen>
+                            </iframe>
                         </Modal.Body>
                     </Modal>
                 </div>
@@ -140,6 +171,5 @@ function App() {
         </div>
     );
 }
-
 
 export default App;
